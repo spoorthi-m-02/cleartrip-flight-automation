aab5af281e3d73f8abe178061d4e8dde703266cd
spoorthi-m-02 <spoomysore@gmail.com>
2020-07-18 16:21:35 +0530

    Added comments for programming test A and B.

--- src/main/java/com/coffeebeans/qa/assignment/tests/ProgrammingTestA.java
+++ src/main/java/com/coffeebeans/qa/assignment/tests/ProgrammingTestA.java
@@ -15,11 +15,18 @@ public class ProgrammingTestA {
 		String prod;
 		List<Character> srcList;
 		List<Character> prodList;
+		// start from 100000 and end at 300000
 		for(int i = 100000; i < 300000; i++) {
+			// convert the number and its product to strings
 			num = ""+i;
 			prod = "" + (i * 2);
+			// get the characters of number and product as list so that we can compare them
 			srcList = num.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
 			prodList = prod.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
+			// initial check to see if characters in product is available in number - not necessarily all characters in number is available in product
+			// if so, then try to compare if all the characters match in each list - by sorting them and comparing each character one by one
+			// we can remove "srcList.containsAll(prodList)" condition and the code still works.. but initial check will avoid sorting of numbers and then returning false
+			// Note: since we are using if condition - if the first part fails then the second part is not evaluated - hence optimised execution
 			if(srcList.containsAll(prodList) && charsMatches(srcList, prodList)) {
 				System.out.println("Result is: " + i + " * 2 = " + prod);
 			}
@@ -27,6 +34,7 @@ public class ProgrammingTestA {
 	}
 
 	private static boolean charsMatches(List<Character> srcList, List<Character> prodList) {
+		// sort the list to be in natural ordering and check if all the characters match
 		Collections.sort(srcList);
 		Collections.sort(prodList);
 		for(int i = 0; i < srcList.size(); i++) {
--- src/main/java/com/coffeebeans/qa/assignment/tests/ProgrammingTestB.java
+++ src/main/java/com/coffeebeans/qa/assignment/tests/ProgrammingTestB.java
@@ -7,30 +7,39 @@ import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class ProgrammingTestB {
-	
+
 	public static void main(String[] args) {
-		String input = "All I need to find is a good answer.";
-//		String input = "Bangalore is in India";
-		for(String s : input.split(" ")) {
-			parseCharCount(s);
+		String sentence = "All I need to find is a good answer.";
+		//String sentence = "Bangalore is in India";
+		// split the sentence by " " empty space to get the word by word
+		for(String word : sentence.split(" ")) {
+			parseCharCount(word);
 		}
 	}
 
-	private static Map<Character, Integer> parseCharCount(String s) {
-		List<Character> charList = s.toLowerCase().chars().mapToObj(e -> (char)e).collect(Collectors.toList());
+	private static Map<Character, Integer> parseCharCount(String word) {
+		// get the characters in the word as list to check the repeating characters
+		// note that we are using toLowerCase() to make case-insensitive characters count
+		List<Character> charList = word.toLowerCase().chars().mapToObj(e -> (char)e).collect(Collectors.toList());
+		// hold the count of characters in a Map
 		Map<Character, Integer> charCountMap = new HashMap<>();
 		Integer count = null;
 		for (Character character : charList) {
 			count = charCountMap.get(character);
 			if(count == null) {
+				// for the first time when the character is encountered, the map does not contain the character and hence returns "null" count
+				// put the count as 1 for first occurrence
 				charCountMap.put(character, 1);
 			} else {
+				// character is repeating.. increment by 1
 				charCountMap.put(character, count + 1);
 			}
 		}
 		for(Entry<Character, Integer> e : charCountMap.entrySet()) {
+			// loop through the entry set of the map
 			if(e.getValue() > 1) {
-				System.out.println("Character \"" + e.getKey() + "\" repeats " + e.getValue() + " times in word \"" + s + "\"");
+				// if character had more that one count, then its repeating one.. print it
+				System.out.println("Character \"" + e.getKey() + "\" repeats " + e.getValue() + " times in word \"" + word + "\"");
 			}
 		}
 		return charCountMap;
